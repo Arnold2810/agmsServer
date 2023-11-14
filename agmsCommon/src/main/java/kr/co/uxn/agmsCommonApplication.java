cfbf0f5022afcdaea4e193e1b1e5084cf415f037
Arnold2810 <kwyeom@uxn.co.kr>
2023-11-14 10:29:23 +0900

    멀티모듈 프로젝트 기본설정 완료

---
+++ agmsCommon/src/main/java/kr/co/uxn/agmsCommonApplication.java
@@ -0,0 +1,12 @@
+package kr.co.uxn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class agmsCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(agmsCommonApplication.class, args);
+    }
+}
